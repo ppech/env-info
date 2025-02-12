016b36ddc45d13a9a4e54fcd2bdb3abf7b2cc04b
ppech <2861692+ppech@users.noreply.github.com>
2025-02-12 14:45:22 +0100

    updated razor package info

--- tools/pack.ps1
+++ tools/pack.ps1
@@ -1,3 +1,4 @@
 dotnet pack "$PSScriptRoot/../src/EnvInfo.Core/EnvInfo.Core.csproj" -c PackRelease
 dotnet pack "$PSScriptRoot/../src/EnvInfo.Mvc/EnvInfo.Mvc.csproj" -c PackRelease
 dotnet pack "$PSScriptRoot/../src/EnvInfo.DotVVM/EnvInfo.DotVVM.csproj" -c PackRelease
+dotnet pack "$PSScriptRoot/../src/EnvInfo.Razor/EnvInfo.Razor.csproj" -c PackRelease
